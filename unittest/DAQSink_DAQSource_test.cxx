4f7f35b4964768b3e086e04382206151c8fa5e23
Kurt Biery <biery@fnal.gov>
2021-03-29 20:13:19 -0500

    Added some tests that illustrate and test how different Queue template types behave

--- unittest/DAQSink_DAQSource_test.cxx
+++ unittest/DAQSink_DAQSource_test.cxx
@@ -26,6 +26,24 @@ using namespace dunedaq::appfwk;
 BOOST_AUTO_TEST_SUITE(DAQSink_DAQSource_test)
 
 /**
+ * @brief Sample base class used in tests
+ */
+class SampleBaseClass
+{
+public:
+  int base_class_value;
+};
+
+/**
+ * @brief Sample derived class used in tests
+ */
+class SampleDerivedClass : public SampleBaseClass
+{
+public:
+  int derived_class_value;
+};
+
+/**
  * @brief Initializes the QueueRegistry
  */
 struct DAQSinkDAQSourceTestFixture
@@ -34,7 +52,10 @@ struct DAQSinkDAQSourceTestFixture
 
   void setup()
   {
-    std::map<std::string, QueueConfig> queue_map = { { "dummy", { QueueConfig::queue_kind::kStdDeQueue, 100 } } };
+    std::map<std::string, QueueConfig> queue_map = { { "dummy", { QueueConfig::queue_kind::kStdDeQueue, 100 } },
+                                                     { "base_class_q", { QueueConfig::queue_kind::kStdDeQueue, 100 } },
+                                                     { "derived_class_q", { QueueConfig::queue_kind::kStdDeQueue, 100 } },
+                                                     { "mixed_class_q", { QueueConfig::queue_kind::kStdDeQueue, 100 } } };
 
     QueueRegistry::get().configure(queue_map);
   }
@@ -104,6 +125,70 @@ BOOST_AUTO_TEST_CASE(Exceptions)
   BOOST_REQUIRE_EXCEPTION(sink.push("bbBbbb"),
                           dunedaq::appfwk::QueueTimeoutExpired,
                           [&](dunedaq::appfwk::QueueTimeoutExpired) { return true; });
+}
+
+BOOST_AUTO_TEST_CASE(SameQueueDifferentType)
+{
+  try {
+    DAQSink<int> sink("dummy");
+    BOOST_TEST_REQUIRE(false, "Test failure: attempt to use an existing Queue with a different type succeeded when we expected it to fail");
+  } catch (...) {}
+  try {
+    DAQSource<int> source("dummy");
+    BOOST_TEST_REQUIRE(false, "Test failure: attempt to use an existing Queue with a different type succeeded when we expected it to fail");
+  } catch (...) {}
+}
+
+BOOST_AUTO_TEST_CASE(BaseClassInstance)
+{
+  int expected_base_class_value = 111;
+
+  DAQSink<SampleBaseClass> sink("base_class_q");
+  DAQSource<SampleBaseClass> source("base_class_q");
+
+  SampleBaseClass sbc;
+  sbc.base_class_value = expected_base_class_value;
+  sink.push(std::move(sbc));
+
+  SampleBaseClass res;
+  res.base_class_value = 2 * expected_base_class_value;
+  try {
+    source.pop(res);
+  } catch (const dunedaq::appfwk::QueueTimeoutExpired& ex) {
+    BOOST_TEST_REQUIRE(false, "Test failure: unexpected timeout exception throw from pop");
+  }
+  BOOST_REQUIRE_EQUAL(res.base_class_value, expected_base_class_value);
+}
+
+BOOST_AUTO_TEST_CASE(DerivedClassInstance)
+{
+  int expected_base_class_value = 123;
+  int expected_derived_class_value = 456;
+
+  DAQSink<SampleDerivedClass> sink("derived_class_q");
+  DAQSource<SampleDerivedClass> source("derived_class_q");
+
+  SampleDerivedClass sdc;
+  sdc.base_class_value = expected_base_class_value;
+  sdc.derived_class_value = expected_derived_class_value;
+  sink.push(std::move(sdc));
+
+  SampleDerivedClass res;
+  res.base_class_value = 2 * expected_base_class_value;
+  res.derived_class_value = 2 * expected_derived_class_value;
+  try {
+    source.pop(res);
+  } catch (const dunedaq::appfwk::QueueTimeoutExpired& ex) {
+    BOOST_TEST_REQUIRE(false, "Test failure: unexpected timeout exception throw from pop");
+  }
+  BOOST_REQUIRE_EQUAL(res.base_class_value, expected_base_class_value);
+  BOOST_REQUIRE_EQUAL(res.derived_class_value, expected_derived_class_value);
+}
+
+BOOST_AUTO_TEST_CASE(MixedClassInstances)
+{
+  DAQSink<SampleDerivedClass*> sink("mixed_class_q");
+  DAQSource<SampleBaseClass*> source("mixed_class_q");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
